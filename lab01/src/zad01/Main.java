6fa8886599e526af0e132942013d11043c9f7e00
piterm11 <piteralke@gmail.com>
2022-03-16 09:13:08 +0100

    Moved display into method

--- lab01/src/zad01/Main.java
+++ lab01/src/zad01/Main.java
@@ -2,6 +2,11 @@ package zad01;
 
 public class Main {
     public static void main(String[] args) {
+        Main main = new Main();
+        main.display();
+    }
+
+    private void display(){
         System.out.println("DOS piterm11");
     }
 }
