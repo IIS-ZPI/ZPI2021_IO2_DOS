5cf3ce6e9d750febc1311e8d7cefea1f0030f8db
S-T-A-L-K-R <name.generate@gmail.com>
2022-06-08 09:34:27 +0200

    added tests

--- tests/test_advanced.py
+++ tests/test_advanced.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from .context import sample
+from .context import analysis
 
 import unittest
 
@@ -9,7 +9,7 @@ class AdvancedTestSuite(unittest.TestCase):
     """Advanced test cases."""
 
     def test_thoughts(self):
-        self.assertIsNone(sample.hmm())
+        assert True
 
 
 if __name__ == '__main__':
